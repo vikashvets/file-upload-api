e505a8609d4a08bdfecbd99b4854563fb9d6f03e
Viktoriia Shvets <viktoriia.shvets.ext@digitalseals.de>
2024-05-13 17:23:41 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+create database file_upload;
+       create table if not exists uploaded_files (
+           id serial primary key,
+           file_name varchar(255),
+           file_size int,
+           file_type varchar(255),
+           file_data bytea,
+           compressed_file_data bytea,
+           compressed_file_size int
+       );
