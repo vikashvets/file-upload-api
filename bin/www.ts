c4c17b4918179c49f79c970b52f45705b4faa8d8
Viktoriia Shvets <vika.shvets1999@gmail.com>
2024-05-23 14:41:31 +0300

    Prepare setup for future enabling of certbot ssl certificate

--- bin/www.ts
+++ bin/www.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import * as fs from "node:fs";
+
 /**
  * Module dependencies.
  */
@@ -7,27 +9,30 @@
 var app = require('../app');
 var debug = require('debug')('file-upload-api:server');
 var http = require('http');
+var https = require('https');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3001');
-app.set('port', port);
+var port = normalizePort(process.env.API_PORT || '3001');
+var httpsPort = normalizePort(process.env.HTTPS_PORT || '443');
 
-/**
- * Create HTTP server.
- */
 
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+if (process.env.NODE_ENV === 'production') {
+  app.set('port', httpsPort);
+  var httpsServer = https.createServer({
+      key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+}, app);
+  httpsServer.listen(httpsPort);
+} else {
+  var server = http.createServer(app);
+  app.set('port', port);
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+}
 
 /**
  * Normalize a port into a number, string, or false.
